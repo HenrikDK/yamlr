2e3e390506f8111143c11a14b42b1d454f29e804
HenrikDK <commander.john.crichton@gmail.com>
2024-11-23 19:15:38 +0100

    rework parser

--- yamale/parser.py
+++ yamale/parser.py
@@ -6,14 +6,26 @@ safe_globals = ("True", "False", "None")
 safe_builtins = dict((f, __builtins__[f]) for f in safe_globals)
 
 
-def _validate_expr(call_node, validators):
+def _extract_expression(call_node, validators):
+    result = {}
     # Validate that the expression uses a known, registered validator.
     try:
+        
         func_name = call_node.func.id
+        
+        kw_args = [{kw.arg: kw.value.value} for kw in call_node.keywords]
+        args = [a.value for a in call_node.args if isinstance(a, ast.Constant)]
+        result = {
+            'name': func_name,
+            'args': args,
+            'kw_args': kw_args,
+            'children': []
+        }
     except AttributeError:
         raise SyntaxError("Schema expressions must be enclosed by a validator.")
     if func_name not in validators:
         raise SyntaxError("Not a registered validator: '%s'. " % func_name)
+    
     # Validate that all args are constant literals, validator names, or other call nodes
     arg_values = call_node.args + [kw.value for kw in call_node.keywords]
     for arg in arg_values:
@@ -23,18 +35,18 @@ def _validate_expr(call_node, validators):
         elif isinstance(base_arg, ast.Name) and base_arg.id in validators:
             continue
         elif isinstance(base_arg, ast.Call):
-            _validate_expr(base_arg, validators)
+            child = _extract_expression(base_arg, validators)
+            result['children'].append(child)
         else:
             raise SyntaxError("Argument values must either be constant literals, or else " "reference other validators.")
+    return result
 
 
 def parse(validator_string, validators=None):
     validators = validators or val.DefaultValidators
     try:
         tree = ast.parse(validator_string, mode="eval")
-        _validate_expr(tree.body, validators)
-        # evaluate with access to a limited global scope only
-        ## TODO: remove eval
-        return eval(compile(tree, "<ast>", "eval"), {"__builtins__": safe_builtins}, validators)
+        result = _extract_expression(tree.body, validators)
+        return result
     except (SyntaxError, NameError, TypeError) as e:
         raise SyntaxError("Invalid schema expression: '%s'. " % validator_string + str(e))
