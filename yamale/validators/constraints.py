3113fdc7c99e29d99ef10369d34b47a9c51bc736
HenrikDK <commander.john.crichton@gmail.com>
2024-11-23 23:35:28 +0100

    more work on new constraints

--- yamale/validators/constraints.py
+++ yamale/validators/constraints.py
@@ -26,7 +26,7 @@ def validate_max(value, constraint, kwargs):
 
 def validate_length_min(value, constraint, kwargs):
     errors = []
-    min = kwargs['min']
+    min = int(kwargs['min'])
     valid = min <= len(value)
     if not valid:
         message = constraint['fail'] % (value, min)
@@ -35,11 +35,146 @@ def validate_length_min(value, constraint, kwargs):
 
 def validate_length_max(value, constraint, kwargs):
     errors = []
-    max = kwargs['max']
+    max = int(kwargs['max'])
     valid = max >= len(value)
     if not valid:
         message = constraint['fail'] % (value, max)
         errors.append(message)
+    return errors
+
+def validate_str_equals(value, constraint, kwargs):
+    errors = []
+    ignore_case = bool(kwargs.get('ignore_case', False))
+    equals = str(kwargs['equals'])
+    
+    valid = True
+    if not ignore_case:
+        valid = value == equals
+    else:
+        valid = value.casefold() == equals.casefold()
+    
+    if not valid:
+        message = constraint['fail'] % (value, min)
+        errors.append(message)
+    
+    return errors
+
+def validate_str_starts_with(value, constraint, kwargs):
+    errors = []
+    ignore_case = bool(kwargs.get('ignore_case', False))
+    starts_with = str(kwargs['starts_with'])
+
+    valid = True
+    if not ignore_case:
+        valid = value.startswith(starts_with)
+    else:
+        length = len(starts_with)
+        if length <= len(value):
+            valid = value[:length].casefold() == starts_with.casefold()
+        else:
+            valid = False
+
+    if not valid:
+        message = constraint['fail'] % (value, min)
+        errors.append(message)
+    
+    return errors
+
+def validate_str_ends_with(value, constraint, kwargs):
+    errors = []
+    ignore_case = bool(kwargs.get('ignore_case', False))
+    ends_with = str(kwargs['ends_with'])
+
+    valid = True
+    if not ignore_case:
+        valid = value.endswith(ends_with)
+    else:
+        length = len(ends_with)
+        if length <= len(value):
+            valid = value[-length:].casefold() == ends_with.casefold()
+        else:
+            valid = False
+    
+    if not valid:
+        message = constraint['fail'] % (value, min)
+        errors.append(message)
+    
+    return errors
+
+def validate_str_matches(value, constraint, kwargs):
+    errors = []
+    matches = str(kwargs['matches'])
+    regex_flags = {"ignore_case": re.I, "multiline": re.M, "dotall": re.S}
+    
+    flags = 0
+    for k, v in util.get_iter(regex_flags):
+        flags |= v if kwargs.pop(k, False) else 0
+
+    valid = True
+    if matches is not None:
+        regex = re.compile(matches, flags)
+        valid = regex.match(value)
+
+    if not valid:
+        message = constraint['fail'] % (value, matches)
+        errors.append(message)
+    
+    return errors
+
+def validate_character_exclude(value, constraint, kwargs):
+    errors = []
+    ignore_case = kwargs.get('ignore_case', False)
+    exclude = str(kwargs['exclude'])
+
+    valid = True
+    failed_char = []
+    for char in exclude:
+        if ignore_case:
+            char = char.casefold()
+            value = value.casefold()
+
+        if char in value:
+            failed_char.append(char)
+            valid = False
+
+    if not valid:
+        message = constraint['fail'] % (value, failed_char)
+        errors.append(message)
+    
+    return errors
+
+def validate_ip_version(value, constraint, kwargs):
+    errors = []
+    version = int(kwargs['version'])
+    
+    valid = True
+    try:
+        ip = ipaddress.ip_interface(to_unicode(value))
+        valid = version == ip.version
+    except ValueError:
+        valid = False
+    
+    if not valid:
+        message = constraint['fail'] % (value, version)
+        errors.append(message)
+    
+    return errors
+
+def validte_key(value, constraint, kwargs):
+    errors = []
+    key = kwargs['key']
+
+    valid = True
+    error_list = []
+    for k in value.keys():
+        val_err = key.validate(k)
+        if val_err != []:
+            error_list.extend(val_err)
+            valid = False
+
+    if not valid:
+        errors = [constraint['fail'] % (e) for e in error_list]
+    
     return errors
 
 constraints = {
@@ -47,6 +182,13 @@ constraints = {
     'max': {'fail': "%s is greater than %s", 'func': validate_max},
     'length_min': {'fail': "Length of %s is less than %s", 'func': validate_length_min},
     'length_max': {'fail': "Length of %s is greater than %s", 'func': validate_length_max},
+    'str_equals': {'fail': "%s does not equal %s", 'func': validate_str_equals},
+    'str_starts_with': {'fail': "%s does not start with %s", 'func': validate_str_starts_with},
+    'str_ends_with': {'fail': "%s does not end with %s", 'func': validate_str_ends_with},
+    'str_matches': {'fail': "%s does not match regex '%s'", 'func': validate_str_matches},
+    'str_char_exclude': {'fail': "'%s' contains excluded character '%s'", 'func': validate_character_exclude},
+    'ip_version': {'fail': "IP version of %s is not %s", 'func': validate_ip_version},
+    'key': {'fail': "Key error - %s", 'func': validte_key}
 }
 
 
@@ -146,23 +288,6 @@ class LengthMax(Constraint):
 
     def _fail(self, value):
         return self.fail % (value, self.max)
-
-
-class Key(Constraint):
-    keywords = {"key": Validator}
-    fail = "Key error - %s"
-
-    def _is_valid(self, value):
-        for k in value.keys():
-            if self.key.validate(k) != []:
-                return False
-        return True
-
-    def _fail(self, value):
-        error_list = []
-        for k in value.keys():
-            error_list.extend(self.key.validate(k))
-        return [self.fail % (e) for e in error_list]
 
 
 class StringEquals(Constraint):
@@ -302,3 +427,21 @@ class IpVersion(Constraint):
 
     def _fail(self, value):
         return self.fail % (value, self.version)
+
+
+class Key(Constraint):
+    keywords = {"key": Validator}
+    fail = "Key error - %s"
+
+    def _is_valid(self, value):
+        for k in value.keys():
+            if self.key.validate(k) != []:
+                return False
+        return True
+
+    def _fail(self, value):
+        error_list = []
+        for k in value.keys():
+            error_list.extend(self.key.validate(k))
+        return [self.fail % (e) for e in error_list]
+
