4c6ca5f7025e6e1434c9d6fd0d16fb34bf436da7
HenrikDK <commander.john.crichton@gmail.com>
2024-11-24 21:50:53 +0100

    change field name

--- yamale/readers.py
+++ yamale/readers.py
@@ -8,7 +8,7 @@ class SafeLineLoader(SafeLoader):
     def construct_mapping(self, node, deep=False):
         mapping = super(SafeLineLoader, self).construct_mapping(node, deep=deep)
         # Add 1 so line numbering starts at 1
-        mapping['__line__'] = node.start_mark.line + 1
+        mapping['_lineno'] = node.start_mark.line + 1
         return mapping
 
 """
