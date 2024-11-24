00625e403c347a03f967f94aaa8cb44701fa8726
HenrikDK <commander.john.crichton@gmail.com>
2024-11-24 17:59:54 +0100

    update test entry point

--- run_subset.py
+++ run_subset.py
@@ -1,13 +1,15 @@
 import yamale, json
+import traceback
 
 try:
     schema = yamale.make_schema('tests/fixtures/keyed/schema_keyed_any_with_include.yaml')
     print(repr(schema))
-    #data = yamale.make_data('tests/fixtures/keyed/data_keyed_any_with_include_bad.yaml')
+    data = yamale.make_data('tests/fixtures/keyed/data_keyed_any_with_include_bad.yaml')
 
-    #yamale.validate(schema, data)
+    yamale.validate(schema, data)
 
     #print('Validation success! 👍')
 except Exception as e:
+    print(traceback.format_exc())
     print('Validation failed!\n%s' % str(e))
     exit(1)
