11e2586fc617151afcb5da1f94dc938625f9dd6e
HenrikDK <commander.john.crichton@gmail.com>
2024-11-25 23:29:28 +0100

    work on fixing tests

--- tests/integration_tests/test_keyed_fixtures.py
+++ tests/integration_tests/test_keyed_fixtures.py
@@ -1,50 +1,45 @@
 import os
 import pytest
 from yamale import YamaleError
-from tests import yamale_testcase as tc
-
+import yamale
 
 def test_keyed_subset_with_include_should_fail_with_correct_message():
-    base_dir = "tests/fixtures/"
-    schema = "keyed/schema_keyed_subset_with_include.yaml"
-    yaml = "keyed/data_keyed_subset_with_include_bad.yaml"
     valid_msg = "workloads.replicas: Unexpected element"
     invalid_msg = "workloads.type: 'api' not in ('ui',)"
     
-    
     with pytest.raises(YamaleError) as excinfo:
-        tc.run_validate(schema, yaml, base_dir)
+        schema = yamale.make_schema("tests/fixtures/keyed/schema_keyed_subset_with_include.yaml")
+        data = yamale.make_data("tests/fixtures/keyed/data_keyed_subset_with_include_bad.yaml")
+        yamale.validate(schema, data)
    
     assert valid_msg in str(excinfo.value)
     assert invalid_msg not in str(excinfo.value)
 
 
 def test_keyed_subset_with_include_should_succeed():
-    base_dir = "tests/fixtures/"
-    schema = "keyed/schema_keyed_subset_with_include.yaml"
-    yaml = "keyed/data_keyed_subset_with_include_good.yaml"
+    schema = yamale.make_schema("tests/fixtures/keyed/schema_keyed_subset_with_include.yaml")
+    data = yamale.make_data("tests/fixtures/keyed/data_keyed_subset_with_include_good.yaml")
+    result = yamale.validate(schema, data)
 
-    result = tc.run_validate(schema, yaml, base_dir)
-    assert result
+    assert len(result) == 0
 
 
 def test_keyed_any_with_include_should_fail_with_correct_message():
-    base_dir = "tests/fixtures/"
-    schema = "keyed/schema_keyed_any_with_include.yaml"
-    yaml = "keyed/data_keyed_any_with_include_bad.yaml"
     valid_msg = "deploy.branch: Unexpected element"
     invalid_msg = "deploy.strategy: 'branch' not in ('preview',)"
 
     with pytest.raises(YamaleError) as excinfo:
-        tc.run_validate(schema, yaml, base_dir)
+        schema = yamale.make_schema("tests/fixtures/keyed/schema_keyed_any_with_include.yaml")
+        data = yamale.make_data("tests/fixtures/keyed/data_keyed_any_with_include_bad.yaml")
+        yamale.validate(schema, data)
     
     assert valid_msg in str(excinfo.value)
     assert invalid_msg not in str(excinfo.value)
 
 
 def test_keyed_any_with_include_should_succeed():
-    base_dir = "tests/fixtures/"
-    schema = "keyed/schema_keyed_any_with_include.yaml"
-    yaml = "keyed/data_keyed_any_with_include_good.yaml"
-    result = tc.run_validate(schema, yaml, base_dir)
-    assert result
+    schema = yamale.make_schema("tests/fixtures/keyed/schema_keyed_any_with_include.yaml")
+    data = yamale.make_data("tests/fixtures/keyed/data_keyed_any_with_include_good.yaml")
+    result = yamale.validate(schema, data)
+
+    assert len(result) == 0
