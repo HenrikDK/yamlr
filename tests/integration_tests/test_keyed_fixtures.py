9d34db4f390b4903ded66f46eb98603f7117e97d
HenrikDK <commander.john.crichton@gmail.com>
2026-05-05 19:28:14 +0200

    add 2 more failing tests

--- tests/integration_tests/test_keyed_fixtures.py
+++ tests/integration_tests/test_keyed_fixtures.py
@@ -3,6 +3,17 @@ import pytest
 from yamlr import YamlrError
 import yamlr
 
+def test_no_match_keyed_subset_should_fail_with_correct_message():
+    with pytest.raises(YamlrError) as e:
+        schema = yamlr.make_schema("tests/fixtures/keyed/schema_keyed_subset_with_include.yaml", debug=True)
+        data = yamlr.make_data("tests/fixtures/keyed/data_keyed_subset_no_match.yaml")
+        yamlr.validate(schema, data)
+
+    errors = e.value.results[0]['errors']
+    assert len(errors) == 1
+    assert errors[0]['error'] == "No match for 'type: hub'"
+    assert errors[0]['path'] == 'workloads'
+
 def test_keyed_subset_with_include_should_fail_with_correct_message():
     with pytest.raises(YamlrError) as e:
         schema = yamlr.make_schema("tests/fixtures/keyed/schema_keyed_subset_with_include.yaml", debug=True)
@@ -25,9 +36,21 @@ def test_keyed_subset_with_include_should_succeed():
     for r in result:
         assert r['is_valid']
 
+def test_no_match_keyed_any_should_fail_with_correct_message():
+    with pytest.raises(YamlrError) as e:
+        schema = yamlr.make_schema("tests/fixtures/keyed/schema_keyed_any_with_include.yaml", debug=True)
+        data = yamlr.make_data("tests/fixtures/keyed/data_keyed_any_no_match.yaml")
+        yamlr.validate(schema, data)
+    
+    errors = e.value.results[0]['errors']
+
+    assert len(errors) == 1
+    assert errors[0]['error'] == "No match for 'strategy: lobster'"
+    assert errors[0]['path'] == 'deploy'
+
 def test_keyed_any_with_include_should_fail_with_correct_message():
     with pytest.raises(YamlrError) as e:
-        schema = yamlr.make_schema("tests/fixtures/keyed/schema_keyed_any_with_include.yaml")
+        schema = yamlr.make_schema("tests/fixtures/keyed/schema_keyed_any_with_include.yaml", debug=True)
         data = yamlr.make_data("tests/fixtures/keyed/data_keyed_any_with_include_bad.yaml")
         yamlr.validate(schema, data)
     
